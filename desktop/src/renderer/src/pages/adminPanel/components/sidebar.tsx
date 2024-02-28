fff69e72243d38e2d0cbc2154b4adbc778b3b1fe
Maciej Monkiewicz <70110809+juanmeister420@users.noreply.github.com>
2024-02-28 19:21:09 +0100

    added sidebar

---
+++ desktop/src/renderer/src/pages/adminPanel/components/sidebar.tsx
@@ -0,0 +1,145 @@
+import { Tooltip, TooltipContent, TooltipTrigger } from '@renderer/shadcn/components/ui/tooltip'
+import { Link } from 'react-router-dom'
+import { cn } from '@renderer/shadcn/components/ui/utils'
+import { buttonVariants } from '@renderer/shadcn/components/ui/button'
+import {
+  AlertCircle,
+  Archive,
+  ArchiveX,
+  File,
+  Inbox,
+  LucideIcon,
+  MessagesSquare,
+  PenBox,
+  Search,
+  Send,
+  ShoppingCart,
+  Trash2,
+  Users2
+} from 'lucide-react'
+import { Separator } from '@renderer/shadcn/components/ui/separator'
+
+interface SidebarLinks {
+  title: string
+  label: string
+  icon: LucideIcon
+  variant: 'default' | 'ghost'
+}
+
+type SidebarLink = SidebarLinks[]
+
+const links: SidebarLink = [
+  {
+    title: 'Inbox',
+    label: '128',
+    icon: Inbox,
+    variant: 'default'
+  },
+  {
+    title: 'Drafts',
+    label: '9',
+    icon: File,
+    variant: 'ghost'
+  },
+  {
+    title: 'Sent',
+    label: '',
+    icon: Send,
+    variant: 'ghost'
+  },
+  {
+    title: 'Junk',
+    label: '23',
+    icon: ArchiveX,
+    variant: 'ghost'
+  },
+  {
+    title: 'Trash',
+    label: '',
+    icon: Trash2,
+    variant: 'ghost'
+  },
+  {
+    title: 'Archive',
+    label: '',
+    icon: Archive,
+    variant: 'ghost'
+  }
+]
+
+export function Sidebar({ isCollapsed }) {
+  return (
+    <div
+      data-collapsed={isCollapsed}
+      className="group flex flex-col gap-4 py-2 data-[collapsed=true]:py-2"
+    >
+      <div
+        className={cn(
+          'flex h-[52px] items-center justify-center',
+          isCollapsed ? 'h-[52px]' : 'px-2'
+        )}
+      >
+        {isCollapsed ? (
+          <h1 className="text-2xl font-bold text-orange-600">
+            m<span className="text-neutral-800">A</span>
+          </h1>
+        ) : (
+          <h1 className="text-2xl font-bold text-orange-600">
+            m<span className="text-neutral-800">Asystent</span>
+          </h1>
+        )}
+      </div>
+      <Separator />
+      <nav className="grid gap-1 px-2 group-[[data-collapsed=true]]:justify-center group-[[data-collapsed=true]]:px-2">
+        {links.map((link, index) =>
+          isCollapsed ? (
+            <Tooltip key={index} delayDuration={0}>
+              <TooltipTrigger asChild>
+                <Link
+                  to="#"
+                  className={cn(
+                    buttonVariants({ variant: link.variant, size: 'icon' }),
+                    'h-9 w-9',
+                    link.variant === 'default' &&
+                      'dark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white'
+                  )}
+                >
+                  <link.icon className="h-4 w-4" />
+                  <span className="sr-only">{link.title}</span>
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent side="right" className="flex items-center gap-4">
+                {link.title}
+                {link.label && <span className="ml-auto text-muted-foreground">{link.label}</span>}
+              </TooltipContent>
+            </Tooltip>
+          ) : (
+            <Link
+              key={index}
+              to="#"
+              className={cn(
+                buttonVariants({ variant: link.variant, size: 'sm' }),
+                link.variant === 'default' &&
+                  'dark:bg-muted dark:text-white dark:hover:bg-muted dark:hover:text-white',
+                'justify-start'
+              )}
+            >
+              <link.icon className="mr-2 h-4 w-4" />
+              {link.title}
+              {link.label && (
+                <span
+                  className={cn(
+                    'ml-auto',
+                    link.variant === 'default' && 'text-background dark:text-white'
+                  )}
+                >
+                  {link.label}
+                </span>
+              )}
+            </Link>
+          )
+        )}
+      </nav>
+    </div>
+  )
+}
--- desktop/src/renderer/src/pages/adminPanel/panel.tsx
+++ desktop/src/renderer/src/pages/adminPanel/panel.tsx
@@ -1,22 +1,70 @@
 import { useAuth } from '@renderer/lib/authContext'
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup
+} from '@renderer/shadcn/components/ui/resizable'
+import { TooltipProvider } from '@renderer/shadcn/components/ui/tooltip'
+import { Sidebar } from './components/sidebar'
+import { useState } from 'react'
+import { cn } from '@renderer/shadcn/components/ui/utils'
 
 function AdminPanel(): JSX.Element {
   const { userDataContext, logout } = useAuth()
 
+  function getCookieValue(a: string) {
+    const b = document.cookie.match('(^|;)\\s*' + a + '\\s*=\\s*([^;]+)')
+    return b ? b.pop() : ''
+  }
+
+  const layout = getCookieValue('react-resizable-panels:layout')
+  const collapsed = getCookieValue('react-resizable-panels:collapsed')
+
+  const defaultLayout = layout ? JSON.parse(layout) : [255, 500]
+  const defaultCollapsed = collapsed ? JSON.parse(collapsed) : false
+
+  const [isCollapsed, setIsCollapsed] = useState(defaultCollapsed)
+
   return (
-    <div className="w-screen h-screen bg-orange-50 flex text-center items-center">
-      <div className="text-6xl font-semibold text-neutral-800 w-full flex flex-col gap-5">
-        <div className="">
-          <span className="text-orange-600">m</span>Asystent - Admin Panel
-          <p className="text-xl font-normal text-neutral-400">Witaj, {userDataContext?.email}!</p>
-          <button
-            className="bg-orange-600 text-white px-3 py-2 rounded-md text-lg"
-            onClick={() => logout()}
+    <div className="w-screen h-screen bg-orange-50 flex flex-col">
+      <TooltipProvider delayDuration={0}>
+        <ResizablePanelGroup
+          direction="horizontal"
+          onLayout={(sizes: number[]) => {
+            document.cookie = `react-resizable-panels:layout=${JSON.stringify(sizes)}`
+          }}
+          className="h-full items-stretch"
+        >
+          <ResizablePanel
+            defaultSize={defaultLayout[0]}
+            collapsedSize={4}
+            collapsible={true}
+            minSize={15}
+            maxSize={20}
+            onCollapse={() => {
+              setIsCollapsed(true)
+              document.cookie = `react-resizable-panels:collapsed=${JSON.stringify(true)}`
+            }}
+            onExpand={() => {
+              setIsCollapsed(false)
+              document.cookie = `react-resizable-panels:collapsed=${JSON.stringify(false)}`
+            }}
+            className={cn(isCollapsed && 'min-w-[50px] transition-all duration-300 ease-in-out')}
           >
-            Wyloguj
-          </button>
-        </div>
-      </div>
+            <Sidebar isCollapsed={isCollapsed} />
+          </ResizablePanel>
+          <ResizableHandle withHandle />
+          <ResizablePanel defaultSize={defaultLayout[1]} minSize={30}>
+            <div className="flex flex-col items-center justify-center h-full">
+              <h1 className="text-4xl font-bold">Admin Panel</h1>
+              <p className="text-2xl">Welcome, {userDataContext?.email}</p>
+              <button onClick={logout} className="mt-4 bg-primary text-white px-4 py-2 rounded-md">
+                Logout
+              </button>
+            </div>
+          </ResizablePanel>
+        </ResizablePanelGroup>
+      </TooltipProvider>
     </div>
   )
 }
